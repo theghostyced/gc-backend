e4c2529ad7ea2041d79235a86f1bef9b4107abc8
Daniel Eze <dan4allu93@gmail.com>
2019-07-15 18:45:44 +0100

    Inital Commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+});
